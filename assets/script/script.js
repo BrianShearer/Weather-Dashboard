883d4e3378b7f2254843554fc34ff32ffbf6320a
Brian Shearer <rrckhunter7@gmail.com>
2021-04-28 10:14:06 -0600

    missed an update

--- assets/script/script.js
+++ assets/script/script.js
@@ -70,20 +70,7 @@ var getForecastApi = function(searchCity) {
   // submitbtn.addEventListener("click", getForecastApi);
 
   function generateForecastCard(data) {
-//     var forecastCard = document.querySelector("#first-day")    
-//     forecastCard.classList.add("#first-day")
-//     var cityName = document.createElement("h1");
-//     cityName.textContent = data.list[0].dt_txt;
-//     forecastCard.append(cityName)
-//     var temp = document.createElement("h3");
-//   temp.textContent = "Temp: "+data.list[0].main.temp;
-//  forecastCard.append(temp);
-//   var wind = document.createElement("h3");
-//   wind.textContent = "Wind: "+data.list[0].wind.speed;
-//  forecastCard.append(wind);
-//   var humidity = document.createElement("h3");
-//   humidity.textContent = "Humidity: "+data.list[0].main.humidity;
-//  forecastCard.append(humidity);
+    
 
  for(var i = 0; i < data.list.length; i+=8){
 
@@ -103,9 +90,7 @@ var getForecastApi = function(searchCity) {
  forecastCard.append(humidity);
  }
     
-// function cityList() {
-//   var newbtn = 
-// }
+
   }
   // var searchCity = cityInputEl.value
   submitbtn.addEventListener("click", getWeatherApi);
